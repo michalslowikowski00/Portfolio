a0370264021bb90db80b20549678d2b32e82abe7
michalslowikowski00 <michal.slowikowski@polidea.com>
2020-09-27 13:32:47 +0200

    added detail template, url and view

--- projects/views.py
+++ projects/views.py
@@ -9,3 +9,14 @@ def all_projects(request):
 
     projects = Project.objects.all()
     return render(request, "projects/all_projects.html", {"projects": projects})
+
+
+def project_detail(request, pk):
+    """
+    :param request: HttpRequest object
+    :type request: Any
+    :param pk: primary key from DB
+    :type pk: int
+    """
+    project = Project.objects.get(pk=pk)
+    return render(request, "projects/detail.html", {"project": project})
