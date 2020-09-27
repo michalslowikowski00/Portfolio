d9c4edfb3349a41cab57e2e9e7e4311967da7aa3
michalslowikowski00 <michal.slowikowski@polidea.com>
2020-09-27 17:08:54 +0200

    added url-linking to album detail page

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 
 from projects import views
 
+app_name = 'projects'
+
 urlpatterns = [
-    path('', views.all_projects),
-    path('<int:pk>', views.project_detail),
+    path('', views.all_projects, name='all_projects'),
+    path('<int:pk>', views.project_detail, name='project_detail'),
 ]
